462086c5009fda90c19129dbd0a19b2c98bcf465
pankaj-deepnapsoftech <itsybizz@gmail.com>
2025-12-29 12:45:21 +0530

    fixing code

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -38,13 +38,7 @@ const routes = [
     element: <MachineStatus />,
     isSublink: false,
   },
-  // {
-  //   name: "Dashboard",
-  //   icon: <MdOutlineSpeed />,
-  //   path: "",
-  //   element: <Dashboard />,
-  //   isSublink: false,
-  // },
+
   {
     name: "Supervisor",
     icon: <IoIosPeople />,
@@ -66,34 +60,7 @@ const routes = [
     element: <MachineHistory />,
     isSublink: false,
   },
-  // {
-  //   name: "Sensors",
-  //   icon: <Component />,
-  //   path: "sensors",
-  //   element: <Sensors />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "Live Data",
-  //   icon: <BarChart3 />,
-  //   path: "Plc-Demo",
-  //   element: <PlcDemo />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "PLC Machine Data",
-  //   icon: <BarChart3 />,
-  //   path: "plc-machine-data",
-  //   element: <PlcMachineData />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "User Roles",
-  //   icon: <TbLockAccess />,
-  //   path: "role",
-  //   element: <UserRole />,
-  //   isSublink: false,
-  // },
+
 
   {
     name: "Resources",
@@ -124,181 +91,7 @@ const routes = [
     element: <StoppageInfo />,
     isSublink: false,
   },
-  //   {
-  //     name: "Inventory",
-  //     icon: <MdOutlineShoppingCart />,
-  //     path: "inventory",
-  //      sublink: [
-  // {
-  //   name: "Dashboard",
-  //   icon: <MdOutlineSpeed />,
-  //   path: "dashboard",
-  //   element: <InventoryDashboard />,
-  // },
-  //      {
-  //        name: "Direct",
-  //        icon: <SlDirection />,
-  //        path: "direct",
-  //        element: <Products />,
-  //      },
-  //     {
-  //       name: "Indirect",
-  //       icon: <FaHandsHelping />,
-  //       path: "indirect",
-  //       element: <IndirectProducts />,
-  //     },
-  //     {
-  //       name: "Work In Progress",
-  //       icon: <GiProgression />,
-  //       path: "wip",
-  //       element: <WIPProducts />,
-  //     },
-  //     {
-  //       name: "Store",
-  //       icon: <Store />,
-  //       path: "store",
-  //       element: <Stores />,
-  //     },
 
-  //     {
-  //       name: "Inventory Approvals",
-  //       icon: <FaRegCheckCircle />,
-  //       path: "approval",
-  //       element: <InventoryApprovals />,
-  //     },
-  //     {
-  //       name: "Scrap Management",
-  //       icon: <SiScrapy />,
-  //       path: "scrap",
-  //       element: <Scrap />,
-  //     },
-  //   ],
-  //   isSublink: true,
-  // },
-  // {
-  //   name: "Sales Order",
-  //   icon: <HandCoins />,
-  //   path: "sales",
-  //   element: <Sales />,
-  //   isSublink: false,
-  // },
-
-  // {
-  //   name: "Procurement",
-  //   icon: <Box />,
-  //   path: "procurement",
-  //   sublink: [
-  //     {
-  //       name: "Purchase Order",
-  //       icon: <ScanBarcode />,
-  //       path: "purchase-order",
-  //       element: <PurchaseOrder />,
-  //     },
-  //   ],
-  //   isSublink: true,
-  // },
-
-  // {
-  //   name: "Production",
-  //   path: "production",
-  //   icon: <MdOutlineProductionQuantityLimits />,
-  //   sublink: [
-  //     {
-  //       name: "Coming Production",
-  //       icon: <Calendar />,
-  //       path: "upcoming-sales",
-  //       element: <UpcomingSales />,
-  //     },
-  //     {
-  //       name: "BOM",
-  //       icon: <RiBillLine />,
-  //       path: "bom",
-  //       element: <BOM />,
-  //     },
-  //     {
-  //       name: "Pre Production",
-  //       icon: <VscServerProcess />,
-  //       path: "pre-production",
-  //       element: <Process />,
-  //     },
-  //     {
-  //       name: "Production Status",
-  //       icon: <VscServerProcess />,
-  //       path: "production-status",
-  //       element: <ProductionStatus />,
-  //     },
-
-  //   ],
-  //   isSublink: true,
-  // },
-  // {
-  //   name: "Dispatch",
-  //   icon: <TbTruckDelivery />,
-  //   path: "dispatch",
-  //   element: <Dispatch />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "Accounts",
-  //   path: "accounts",
-  //   icon: <BiPurchaseTagAlt />,
-  //   sublink: [
-  // {
-  //   name: "Dashboard",
-  //   icon: <MdOutlineSpeed />,
-  //   path: "dashboard",
-  //   element: <AccountantDashboard />,
-  // },
-  // {
-  //   name: "Dashboard",
-  //   icon: <MdOutlineSpeed />,
-  //   path: "dashboard",
-  //   element: <AccountantDashboard />,
-  // },
-  //     {
-  //       name: "Proforma Invoices",
-  //       icon: <IoDocumentTextOutline />,
-  //       path: "proforma-invoice",
-  //       element: <ProformaInvoice />,
-  //     },
-  //     {
-  //       name: "Tax Invoices",
-  //       icon: <RiBillLine />,
-  //       path: "taxInvoice",
-  //       element: <Invoice />,
-  //     },
-
-  //     {
-  //       name: "Payments",
-  //       icon: <MdOutlinePayment />,
-  //       path: "payment",
-  //       element: <Payment />,
-  //     },
-  //   ],
-  //   isSublink: true,
-  // },
-
-  // {
-  //   name: "Task",
-  //   icon: <MdTask />,
-  //   path: "task",
-  //   element: <Task />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "Designer Dashboard",
-  //   icon: <MdOutlineSpeed />,
-  //   path: "designer-dashboard",
-  //   element: <DesignerDashboard />,
-  //   isSublink: false,
-  // },
-  // {
-  //   name: "Control Panel",
-  //   icon: <FaRegCheckCircle />,
-  //   path: "approval",
-  //   element: <Approvals />,
-  //   isSublink: false,
-  // },
   {
     name: "My Profile",
     icon: <CgProfile />,
